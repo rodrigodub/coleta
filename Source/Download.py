c0272a0636989cab0f05a8d52d7d4890c16dcda2
rodrigodub <Rodrigo@coltrane.bigpond>
2013-09-15 22:06:38 +0800

    Class Download - essential logic OK

--- Source/Download.py
+++ Source/Download.py
@@ -2,32 +2,56 @@
 ## Download
 ## Routine to get Data to Work on
 ## 
-## v0.1.0
+## v0.1.1
 ## for ticket ID43
 ## 
 ## Rodrigo Nobrega
-## 20130914-
+## 20130914-20130915
 #################################################
 
 
 # class Download
 class Download(object):
-	"""Define the Download dta to go offline"""
+	"""Define the Download data to go offline"""
 
 	# constructor with attributes
-	def __init__(self, fileNameTest):
+	def __init__(self):
 		super(Download, self).__init__()
-		self.fileNameTest = fileNameTest
+		#self.fileName = fileName
+		self.dictOfTables = {"Hole":"", "Survey":"", "Interval1":"", "Interval2":"", "Interval3":"", "Interval4":"", "Interval5":"", "Samples":""}
+		self.dictOfFields = {"Hole":[], "Survey":[], "Interval1":[], "Interval2":[], "Interval3":[], "Interval4":[], "Interval5":[], "Samples":[]}
 
-	# parseFile / parseFileTest
-	def parseFileTest(self):
+
+	# parse file / parseFile
+	def parseFile(self,dictkey):
 		# open
-		arq = open(self.fileNameTest,'r')
+		arq = open(self.dictOfTables[dictkey],'r')
 		# iterate
 		for eachLine in arq:
 			print eachLine
 		# close
 		arq.close()
+
+
+	# define tables source / setTableSource
+	def setTableSource(self,dictkey,fileName):
+		self.dictOfTables[dictkey] = fileName
+
 		
+	# get field names / getFieldNames
+	def getFieldNames(self,dictkey):
+		# open
+		arq = open(self.dictOfTables[dictkey],'r')
+		# get first line
+		fieldList = arq.readline().replace("\r\n","").split(",")
+		# return fieldList
+		return fieldList
+		# close
+		arq.close()
+
+
+	# set fields list for the table / setFieldsList
+	def setFieldsList(self,dictkey):
+		self.dictOfFields[dictkey] = self.getFieldNames(dictkey)
 
 
--- Source/MainTest.py
+++ Source/MainTest.py
@@ -4,11 +4,11 @@
 ## Main (Test)
 ## Main routine / test
 ## 
-## v0.1.0
+## v0.1.1
 ## 
 ## 
 ## Rodrigo Nobrega
-## 20130914
+## 20130914-20130915
 #################################################
 
 
@@ -18,17 +18,43 @@ from Download import *
 
 
 # main loop
-def main():  
-	fnam = '../Test/acQuire Data/01 All Collar.csv' #03 Lithology.csv'
-	colar = Download(fnam)
-	print colar.fileNameTest
-	colar.parseFileTest()
+def main():
+	# prototype source of tables: csv files  
+	file1 = '../Test/acQuire Data/01 All Collar.csv' 
+	file2 = '../Test/acQuire Data/02 Survey.csv'
+	file3 = '../Test/acQuire Data/03 Lithology.csv'
+	file4 = '../Test/acQuire Data/04 Alteration.csv'
+	file5 = '../Test/acQuire Data/05 Samples Checks.csv'
+	# create instance: session
+	session = Download()
+	# define source tables
+	session.setTableSource("Hole", file1)
+	session.setTableSource("Survey", file2)
+	session.setTableSource("Interval1", file3)
+	session.setTableSource("Interval2", file4)
+	session.setTableSource("Samples", file5)
+	# get the fields list
+	session.setFieldsList("Hole")
+	session.setFieldsList("Survey")
+	session.setFieldsList("Interval1")
+	session.setFieldsList("Interval2")
+	session.setFieldsList("Samples")
+	# test
+	# print session.getFieldNames("Samples")
+	# print session.dictOfFields["Hole"][-1]
+	for fld in session.dictOfFields["Survey"]:
+		print fld
+
+
+	#print colar.fileNameTest
+	#colar.parseFileTest()
+	#colar.getFieldNamesTest()
 	
  
 
 # main, calling main loop
 if __name__ == '__main__':
-    main()  
+	main()  
 
 
 
